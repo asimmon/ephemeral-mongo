cc9aa7767b93523a4925f75f22f2586e02939b5d
Anthony Simmon <anthony.simmon@workleap.com>
2025-04-09 23:10:51 -0400

    sync APIs

--- src/EphemeralMongo/IMongoRunner.cs
+++ src/EphemeralMongo/IMongoRunner.cs
@@ -6,5 +6,9 @@ public interface IMongoRunner : IDisposable
 
     Task ImportAsync(string database, string collection, string inputFilePath, string[]? additionalArguments = null, bool drop = false, CancellationToken cancellationToken = default);
 
+    void Import(string database, string collection, string inputFilePath, string[]? additionalArguments = null, bool drop = false);
+
     Task ExportAsync(string database, string collection, string outputFilePath, string[]? additionalArguments = null, CancellationToken cancellationToken = default);
+
+    void Export(string database, string collection, string outputFilePath, string[]? additionalArguments = null);
 }
